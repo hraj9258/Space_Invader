0748a9b0184c37502cc968f5dadc478455b048f9
Himanshu Raj <himanshurajkush@gmail.com>
2020-05-10 22:31:44 +0530

    Added: title, icon & background

--- main.py
+++ main.py
@@ -6,9 +6,18 @@ pygame.init()
 #creating the pygame screen
 screen = pygame.display.set_mode((800, 600))
 
+# Title and Icon
+pygame.display.set_caption("Space Invader!")
+logo = pygame.image.load("icon.ico")
+pygame.display.set_icon(logo)
+
 # starting the gameloop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    #RGB (red, green, blue)
+    screen.fill((0, 125, 125))
+    pygame.display.update()

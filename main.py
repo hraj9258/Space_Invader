17b16e44204dc77bf061371ce4b6220d906285fd
Himanshu Raj <himanshurajkush@gmail.com>
2020-05-13 20:32:41 +0530

    Added: Background

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ pygame.init()
 # creating the pygame screen
 screen = pygame.display.set_mode((800, 600))  # 800 is width and 600 is height
 
+# background
+background = pygame.image.load("background.png")
+
 # Title and Icon
 pygame.display.set_caption("Space Invader!")
 logo = pygame.image.load("logo.png")
@@ -22,10 +25,10 @@ playerX_change = 0
 enemyImg = pygame.image.load("enemy.png")
 enemyX = random.randint(0, 370)
 enemyY = random.randint(50, 150)
-enemyX_change = 0.5
+enemyX_change = 4
 enemyY_change = 40
 
-
+ddddddd
 def player(x, y):
     screen.blit(playerImg, (x, y))  # blit is used to show the image
 
@@ -39,6 +42,8 @@ running = True
 while running:
     # RGB (red, green, blue)
     screen.fill((0, 125, 125))
+    # background image
+    screen.blit(background, (0, 0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -47,9 +52,9 @@ while running:
         # if keystroke is pressed check weather it is for right or left
         if event.type == pygame.KEYDOWN:  # checks for key press
             if event.key == pygame.K_a:
-                playerX_change = -0.5
+                playerX_change = -5
             if event.key == pygame.K_d:
-                playerX_change = 0.5
+                playerX_change = 5
         if event.type == pygame.KEYUP:  # check for key release
             if event.key == pygame.K_a or event.key == pygame.K_d:
                 playerX_change = 0  # stops the movement of player
@@ -65,10 +70,10 @@ while running:
     enemyX += enemyX_change
 
     if enemyX <= 0:
-        enemyX_change = 0.5
+        enemyX_change = 4
         enemyY += enemyY_change
     elif enemyX >= 736:
-        enemyX_change = -0.5
+        enemyX_change = -4
         enemyY += enemyY_change
 
     player(playerX, playerY)
